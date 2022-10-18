531d1980fb21a92d6abd61bc1a1ecf638faa2247
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-17 20:22:33 -0400

    done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,14 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    def give_freebie(dev, item, value)
+        Freebie.create(item_name: item, value: value, dev: dev, company_id: self.id)
+    end
+
+    def self.oldest_company
+        self.order(:founding_year).first
+        # or .limit(1) return an array with 1 result [0] to eliminate the array
+    end
 
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,19 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item)
+     self.freebies.any?{ |freebie| freebie.item_name.downcase == item.downcase }
+        # self.freebies.find { |freebie| freebie.item_name == item} + define in var w cond true/false
+    end
+
+    def give_away(dev, freebie)
+        # if freebies dev id equals the devs id, dev should own that freebie
+        if freebie.dev_id == self.id
+            # reassign the freebie to the arg dev by their id
+            freebie.dev_id = dev.id
+            # save to persist 
+            freebie.save
+        end
+    end
 end
